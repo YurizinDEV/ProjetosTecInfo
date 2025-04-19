2ae0ea47c65485b3c562706f1bf92566334898a0
YurizinDEV <yurizetoles0123@gmail.com>
2025-04-19 14:21:31 -0300

    fix: corrigir caminhos de arquivos de imagem e música

--- radio/assets/script.js
+++ radio/assets/script.js
@@ -133,8 +133,8 @@ function likeButtonRender() {
 }
 
 function initializeSong() {
-  cover.src = `images/${sortedPlaylist[index].file}.jpg`;
-  song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+  cover.src = `assets/images/${sortedPlaylist[index].file}.jpg`;
+  song.src = `assets/music/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
   likeButtonRender();
